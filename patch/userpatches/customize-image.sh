f7c47f122bb87f3b349a3bd9f9ee1ed9b26f9a7a
hy5528 <95360525+hy5528@users.noreply.github.com>
2025-06-01 21:57:59 +0800

    Update customize-image.sh

--- patch/userpatches/customize-image.sh
+++ patch/userpatches/customize-image.sh
@@ -4,14 +4,14 @@ sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
  
 # 更新sources.list文件
 sudo tee /etc/apt/sources.list <<EOF
-deb https://mirrors.aliyun.com/debian/ bookworm main non-free non-free-firmware contrib 
-deb-src https://mirrors.aliyun.com/debian/ bookworm main non-free non-free-firmware contrib 
-deb https://mirrors.aliyun.com/debian-security/ bookworm-security main 
-deb-src https://mirrors.aliyun.com/debian-security/ bookworm-security main 
-deb https://mirrors.aliyun.com/debian/ bookworm-updates main non-free non-free-firmware contrib 
-deb-src https://mirrors.aliyun.com/debian/ bookworm-updates main non-free non-free-firmware contrib 
-deb https://mirrors.aliyun.com/debian/ bookworm-backports main non-free non-free-firmware contrib 
-deb-src https://mirrors.aliyun.com/debian/ bookworm-backports main non-free non-free-firmware contrib
+deb https://mirrors.163.com/debian/ bookworm main non-free non-free-firmware contrib
+deb-src https://mirrors.163.com/debian/ bookworm main non-free non-free-firmware contrib
+deb https://mirrors.163.com/debian-security/ bookworm-security main
+deb-src https://mirrors.163.com/debian-security/ bookworm-security main
+deb https://mirrors.163.com/debian/ bookworm-updates main non-free non-free-firmware contrib
+deb-src https://mirrors.163.com/debian/ bookworm-updates main non-free non-free-firmware contrib
+deb https://mirrors.163.com/debian/ bookworm-backports main non-free non-free-firmware contrib
+deb-src https://mirrors.163.com/debian/ bookworm-backports main non-free non-free-firmware contrib
 EOF
 # 更新软件包
 sudo apt-get update
