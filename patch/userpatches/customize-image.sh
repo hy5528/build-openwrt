d9e427008e30085d72d17b144cd3a712d6eaeef0
hy5528 <95360525+hy5528@users.noreply.github.com>
2025-06-02 19:52:23 +0800

    Update customize-image.sh

--- patch/userpatches/customize-image.sh
+++ patch/userpatches/customize-image.sh
@@ -15,12 +15,8 @@ deb-src https://mirrors.163.com/debian/ bookworm-backports main non-free non-fre
 EOF
 # 更新软件包
 sudo apt-get update
-sudo apt dist-upgrade -y
 
-# 安装 Docker
-sudo apt-get install -y ca-certificates curl gnupg lsb-release alsa-utils 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo systemctl start docker
+
 
 # 清理缓存
 apt autoremove -y && apt autoclean && apt remove -y && apt clean
