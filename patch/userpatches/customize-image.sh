b3983e354babb00d0e796e48b00c2a7f07ead91c
hy5528 <95360525+hy5528@users.noreply.github.com>
2025-06-03 17:49:03 +0800

    Update customize-image.sh

--- patch/userpatches/customize-image.sh
+++ patch/userpatches/customize-image.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-# 备份原有的sources.list文件
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
- 
-# 更新sources.list文件
-sudo tee /etc/apt/sources.list <<EOF
-deb https://mirrors.163.com/debian/ bookworm main non-free non-free-firmware contrib
-deb-src https://mirrors.163.com/debian/ bookworm main non-free non-free-firmware contrib
-deb https://mirrors.163.com/debian-security/ bookworm-security main
-deb-src https://mirrors.163.com/debian-security/ bookworm-security main
-deb https://mirrors.163.com/debian/ bookworm-updates main non-free non-free-firmware contrib
-deb-src https://mirrors.163.com/debian/ bookworm-updates main non-free non-free-firmware contrib
-deb https://mirrors.163.com/debian/ bookworm-backports main non-free non-free-firmware contrib
-deb-src https://mirrors.163.com/debian/ bookworm-backports main non-free non-free-firmware contrib
-EOF
+
 # 更新软件包
 sudo apt-get update
 
